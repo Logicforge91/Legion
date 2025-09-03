5d5f12147ce6bdcb2a12d3cc941d07711ce37a73
sumanks1307 <sumanks1307@gmail.com>
2025-09-03 19:58:12 +0530

    changes

--- js/layout.js
+++ js/layout.js
@@ -1,10 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // Load Header
-  fetch("header.html")
-    .then(res => res.text())
-    .then(html => {
-      document.getElementById("headerContainer").innerHTML = html;
-    });
+
 
   // Load Footer
   fetch("footer.html")
