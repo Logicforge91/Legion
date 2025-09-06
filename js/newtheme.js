c8f49777261a611215e1e1571905ae691b41b8fc
sumanks1307 <sumanks1307@gmail.com>
2025-09-06 19:55:06 +0530

    new style

---
+++ js/newtheme.js
@@ -0,0 +1,14 @@
+
+
+    /* === Header Section  Start === */
+
+document.addEventListener("DOMContentLoaded", () => {
+  const menuBtn = document.getElementById("menu-btn");
+  const mobileNav = document.getElementById("mobile-nav");
+
+  menuBtn.addEventListener("click", () => {
+    mobileNav.classList.toggle("active");
+  });
+});
+
+  /* === Header Section  End === */
